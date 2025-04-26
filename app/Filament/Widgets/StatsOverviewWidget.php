71e74bab10a334b950583b45af25294c48343ef8
Mario Juarez <mario@mjp.one>
2025-04-27 00:15:20 +0200

    Remove stats descriptions

--- app/Filament/Widgets/StatsOverviewWidget.php
+++ app/Filament/Widgets/StatsOverviewWidget.php
@@ -10,32 +10,23 @@ use Filament\Widgets\StatsOverviewWidget\Stat;
 class StatsOverviewWidget extends BaseWidget
 {
     protected static ?int $sort = 1;
-    
+
     protected function getStats(): array
     {
         $totalInterviews = Interview::count();
         $totalSessions = InterviewSession::count();
         $completedSessions = InterviewSession::where('finished', true)->count();
-        
-        $completionRate = $totalSessions > 0 
-            ? round(($completedSessions / $totalSessions) * 100, 1) 
+
+        $completionRate = $totalSessions > 0
+            ? round(($completedSessions / $totalSessions) * 100, 1)
             : 0;
-            
+
         return [
-            Stat::make('Total Interviews', $totalInterviews)
-                ->description('Configured interviews')
-                ->descriptionIcon('heroicon-m-document-text')
-                ->color('success'),
-                
-            Stat::make('Total Sessions', $totalSessions)
-                ->description('Interview sessions')
-                ->descriptionIcon('heroicon-m-chat-bubble-left-right')
-                ->color('primary'),
-                
-            Stat::make('Completion Rate', $completionRate . '%')
-                ->description($completedSessions . ' completed sessions')
-                ->descriptionIcon('heroicon-m-check-circle')
-                ->color('info'),
+            Stat::make('Total Interviews', $totalInterviews),
+
+            Stat::make('Total Sessions', $totalSessions),
+
+            Stat::make('Completion Rate', $completionRate . '%'),
         ];
     }
-} 
+}
