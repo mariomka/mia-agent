6c0807e3357420af99e68f06a1c9ba81dca8293c
Mario Juarez <mario@mjp.one>
2025-04-27 11:06:28 +0200

    Add rate limiting to interview and chat routes

--- routes/web.php
+++ routes/web.php
@@ -8,6 +8,10 @@ Route::get('/', function () {
     return '';
 })->name('home');
 
-Route::get('/interviews/{interview}', InterviewController::class)->name('interview');
+Route::get('/interviews/{interview}', InterviewController::class)
+    ->middleware(['throttle:20,1']) // 20 requests per minute
+    ->name('interview');
 
-Route::post('/chat', ChatController::class);
+Route::post('/chat', ChatController::class)
+    ->middleware(['throttle:60,1']) // 60 requests per minute
+    ->name('chat');
